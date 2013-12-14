485135cf2f915ab23f1148e81e1629b5b057f678
Alexander "Ace" Olofsson <ace@haxalot.com>
2013-12-15 00:51:28 +0100

    TODO: Add particles

---
+++ src/Game/Particle.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+/*
+
+######   ###   ###    ###
+  #     #   #  #  #  #   #
+  #     #   #  #  #  #   #
+  #     #   #  #  #  #   #
+  #      ###   ###    ###
+
+*/
+
+#include <Kunlaboro/Component.hpp>
+
+class Particle
+{
+public:
+    Particle();
+    ~Particle();
+
+    void addedToEntity();
+
+private:
+    
+};
